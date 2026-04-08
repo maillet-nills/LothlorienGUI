bf7b0402b375868ac7ff59247ac29a5457f58373
Nills Maillet <mailletnills@gmail.com>
2026-04-08 19:39:54 +0200

    feat: first Lothlórien gui design
    - add layout design (nav panel & main panel)
    - add nav-items with icons (sections) to nav panel
    - add "collection widget" in nav panel
    - add "my garden" section with title & card/list view toggle buttons

--- LothlorienGUI/ViewModels/MainWindowViewModel.cs
+++ LothlorienGUI/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,17 @@
-﻿namespace LothlorienGUI.ViewModels;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace LothlorienGUI.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting { get; } = "Welcome to Lothlorien !";
+    // Nombre de plantes (0 pour l'instant, sera dynamique plus tard)
+    public int PlantCount => 0;
+
+    // L'attribut [ObservableProperty] génère automatiquement IsCardView 
+    // et gère la notification de changement (INotifyPropertyChanged)
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(IsListView))] // Notifie IsListView quand IsCardView change
+    private bool _isCardView = true;
+
+    public bool IsListView => !IsCardView;
 }
