7926588dc7355cc8fc4aa32047c725e30c9965e5
Nills Maillet <mailletnills@gmail.com>
2026-04-08 21:46:02 +0200

    feat: add AddPlantWindow and AddOnPlantButton event

---
+++ LothlorienGUI/Views/AddPlantWindow.axaml.cs
@@ -0,0 +1,14 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace LothlorienGUI.Views;
+
+public partial class AddPlantWindow : Window
+{
+    
+    public AddPlantWindow()
+    {
+        InitializeComponent();
+    }
+}
--- LothlorienGUI/Views/MainWindow.axaml.cs
+++ LothlorienGUI/Views/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace LothlorienGUI.Views;
 
@@ -7,5 +8,12 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private async void AddOnPlantButton_OnClick(object? sender, RoutedEventArgs e)
+    {
+        var plantInputWindow = new AddPlantWindow();
+        
+        await plantInputWindow.ShowDialog(this);
     }
 }
